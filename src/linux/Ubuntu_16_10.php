a109aadfe8e694546984d2bc82342be36fa626f7
Chris Thomas <christopher.thomas@plista.com>
2020-07-31 15:47:49 +0200

    implement support for ubuntu 16.10 and update 18.04
    Signed-off-by: Chris Thomas <christopher.thomas@plista.com>

---
+++ src/linux/Ubuntu_16_10.php
@@ -0,0 +1,2 @@
+<?php
+class Ubuntu_16_10 extends Ubuntu_18_04 {}
--- src/linux/Ubuntu_18_04.php
+++ src/linux/Ubuntu_18_04.php
@@ -6,33 +6,9 @@ class Ubuntu_18_04
 
     }
 
-    public function enableDNS(string $ipAddress): bool
+    public function enableDNS(): bool
     {
-        $name = $ipAddress === 'empty' ? 'Reset back to router' : $ipAddress;
-        $name = $ipAddress === 'docker' ? 'Docker Container' : $name;
-
         $ipAddress = '0.0.0.0';
-        
-        if(empty($ipAddress)){
-            Text::print("{red}Configuration error: ip address must not be empty{end}\n");
-            return false;
-        }
-
-        $resolvedConf = "/etc/systemd/resolved.conf";
-
-        if(!file_exists("$resolvedConf")){
-            Text::print("{red}Cannot find '$resolvedConf' in file system, cannot continue{end}\n");
-            return false;
-        }
-
-        // I hate that these are similar names, but what else should I call it? 
-        $resolvConf = "/run/systemd/resolve/resolv.conf";
-
-        if(!file_exists("$resolvConf")){
-            Text::print("{red}Cannot find '$resolvConf' in file system, cannot continue{end}\n");
-            return false;
-        }
-
 
         Text::print("{blu}DNS:{end} Writing new DNS Configuration\n");
         Shell::exec('sudo sed -i "s/^[#]\?DNS=.*\?/DNS='.$ipAddress.'/i" /etc/systemd/resolved.conf');
@@ -45,29 +21,9 @@ class Ubuntu_18_04
         return true;
     }
 
-    public function disableDNS(string $ipAddress): bool
+    public function disableDNS(): bool
     {
-        if(empty($ipAddress)){
-            Text::print("{red}Configuration error: ip address must not be empty{end}\n");
-            return false;
-        }
-
-        $ipAddress = '0.0.0.0';
-
-        $resolvedConf = "/etc/systemd/resolved.conf";
-
-        if(!file_exists("$resolvedConf")){
-            Text::print("{red}Cannot find '$resolvedConf' in file system, cannot continue{end}\n");
-            return false;
-        }
-
-        // I hate that these are similar names, but what else should I call it? 
-        $resolvConf = "/run/systemd/resolve/resolv.conf";
-
-        if(!file_exists("$resolvConf")){
-            Text::print("{red}Cannot find '$resolvConf' in file system, cannot continue{end}\n");
-            return false;
-        }
+        $ipAddress = '127.0.0.1';
 
         Text::print("{blu}DNS:{end} Resetting DNS Configuration back to sensible defaults\n");
         Shell::exec('sudo sed -i "s/^DNS='.$ipAddress.'/#DNS=/i" /etc/systemd/resolved.conf');
@@ -75,9 +31,6 @@ class Ubuntu_18_04
 
         Text::print("{blu}DNS:{end} Restarting 'systemd-resolved' to set DNS to use default resolver\n");
         Shell::exec("sudo systemctl restart systemd-resolved");
-
-        //Shell::exec("sudo kill -9 $(ps aux | grep [N]etworkManager/dnsmasq | awk '{ print $2 }')");
-        //Shell::exec("sudo kill -9 $(ps aux | grep [d]nsmasq | awk '{ print $2 }')");
 
         return true;
     }
