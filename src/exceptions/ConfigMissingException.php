ceb265f3dc7a8a5a7de9a1ee3a9f59554621e9d5
Chris Thomas <chris.thomas@antimatter-studios.com>
2020-09-22 13:23:44 +0200

    update extensions

--- src/exceptions/ConfigMissingException.php
+++ src/exceptions/ConfigMissingException.php
@@ -2,6 +2,6 @@
 class ConfigMissingException extends Exception{
     public function __construct(string $filename, int $code = 0, Throwable $previous=null)
     {
-        parent::__construct("The Configuration '$filename' was missing", $code, $previous);
+        parent::__construct("The Configuration file named '$filename' or a file inside path '$filename' could not be found", $code, $previous);
     }
 }
---
+++ src/exceptions/ConfigWrongTypeException.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+class ConfigWrongTypeException extends Exception{
+    public function __construct(array $type, int $code = 0, Throwable $previous=null)
+    {
+        parent::__construct("The Configuration was type '{$type[0]}' but should have been type '{$type[1]}'", $code, $previous);
+    }
+}
