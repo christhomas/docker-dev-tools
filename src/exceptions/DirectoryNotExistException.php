503000e4def1543e083525719a07ecbbbe2b0736
Chris Thomas <chris.thomas@antimatter-studios.com>
2020-09-22 13:23:34 +0200

    rename this exception

---
+++ src/exceptions/DirectoryNotExistException.php
@@ -0,0 +1,6 @@
+<?php
+class DirectoryNotExistException extends Exception{
+    public function __construct(string $dir, $code = 0, \Throwable $previous = null){
+        parent::__construct("The directory '$dir' does not exist", $code, $previous);
+    }
+};
