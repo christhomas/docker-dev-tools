7cac66765b6e847a38a238e7f75618c8b43a365e
Chris Thomas <chris.thomas@antimatter-studios.com>
2021-08-09 14:29:05 +0200

    refactor the cli to get rid of junk functionality and streamline what I really need

--- src/CLI.php
+++ src/CLI.php
@@ -6,20 +6,21 @@ class CLI
 	private $args = [];
 	private $name = null;
 
-	public function __construct(array $argv, bool $showErrors=false)
+	public function __construct(array $argv)
 	{
-		if($showErrors){
-			$this->enableErrors();
-		}
-
 		$this->setName($argv[0]);
 		$this->setArgs(array_slice($argv, 1));
 	}
 
-	public function enableErrors()
+	public function enableErrors(bool $showErrors=false)
 	{
-		error_reporting(-1);
-		ini_set("display_errors", 'true');
+		if($showErrors){
+			error_reporting(-1);
+			ini_set('display_errors', 'true');
+		}else{
+			error_reporting(0);
+			ini_set('display_errors', 'false');
+		}
 	}
 
 	public function setName(string $name): void
@@ -32,16 +33,12 @@ class CLI
 		return $withPath ? $this->name : basename($this->name);
 	}
 
-	/**
-	 *	FIXME: I don't like that this function is here, it's a copy of another function in the base config object
-	 * 	NOTE: it's also nothing to do with the CLI really, so it's awkward to have it here
-	 * @param string|null $subpath
-	 * @return string
-	 * @deprecated
-	 */
-	static public function getToolPath(string $subpath=null): string
+	static public function ask(string $question, array $accept): string
 	{
-		return dirname(__DIR__) . $subpath;
+		$responses = "(Accepts: " . implode(", ", $accept) . "): ";
+		$reply = readline(\Text::write("{yel}$question $responses{end}"));
+
+		return $reply;
 	}
 
 	public function setArgs(array $argv): array
@@ -51,10 +48,20 @@ class CLI
 		foreach($argv as $v){
 			$v = explode('=', $v);
 
-			$this->args[] = ['name' => trim(array_shift($v), '-'), 'value' => array_shift($v)];
+			$this->args[] = ['name' => array_shift($v), 'value' => array_shift($v)];
 		}
 
 		return $this->args;
+	}
+
+	public function shiftArg(): ?array
+	{
+		return array_shift($this->args);
+	}
+
+	public function countArgs(): int
+	{
+		return count($this->args);
 	}
 
 	public function getArgList(): array
@@ -73,28 +80,6 @@ class CLI
 		return $default;
 	}
 
-	public function setArg(string $name, $value=null): void
-	{
-		$this->args[] = ['name' => $name, 'value' => $value];
-	}
-
-	public function getArgWithVal(string $name, $default=null): ?string
-	{
-		$arg = $this->getArg($name, $default);
-
-		return $arg != "true" ? $arg : $default;
-	}
-
-	public function getArgByIndex(int $index): ?string
-	{
-		return array_key_exists($index, $this->args) ? $this->args[$index]['value'] : null;
-	}
-
-	public function shiftArg(): ?array
-	{
-		return array_shift($this->args);
-	}
-
 	public function hasArg($name): ?bool
 	{
 		if(is_string($name)) $name = [$name];
@@ -109,16 +94,44 @@ class CLI
 		return true;
 	}
 
-	public function countArgs(): int
+
+	// DEPRECATED FUNCTIONALITY BELOW, TRY TO NOT USE ANY OF THE FOLLOWING METHODS
+
+	/**
+	 *	FIXME: I don't like that this function is here, it's a copy of another function in the base config object
+	 * 	NOTE: it's also nothing to do with the CLI really, so it's awkward to have it here
+	 * @param string|null $subpath
+	 * @return string
+	 * @deprecated
+	 */
+	static public function getToolPath(string $subpath=null): string
 	{
-		return count($this->args);
+		return dirname(__DIR__) . $subpath;
 	}
 
-	static public function ask(string $question, array $accept): string
+	/**
+	 * @deprecated
+	 */
+	public function setArg(string $name, $value=null): void
 	{
-		$responses = "(Accepts: " . implode(", ", $accept) . "): ";
-		$reply = readline(\Text::write("{yel}$question $responses{end}"));
+		$this->args[] = ['name' => $name, 'value' => $value];
+	}
 
-		return $reply;
+	/**
+	 * @deprecated
+	 */
+	public function getArgWithVal(string $name, $default=null): ?string
+	{
+		$arg = $this->getArg($name, $default);
+
+		return $arg != "true" ? $arg : $default;
+	}
+
+	/**
+	 * @deprecated
+	 */
+	public function getArgByIndex(int $index): ?string
+	{
+		return array_key_exists($index, $this->args) ? $this->args[$index]['value'] : null;
 	}
 }
