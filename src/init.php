5d0c9cb1ae11513800c8bbaa4bc2dd3368d36c0e
Chris Thomas <chris.thomas@antimatter-studios.com>
2021-08-27 06:41:45 +0200

    support the new container

--- src/init.php
+++ src/init.php
@@ -1,27 +1,6 @@
 <?php declare(strict_types=1);
 
-require_once(__DIR__ . '/container.php');
 require_once(__DIR__ . '/autoload.php');
+require_once(__DIR__ . '/services.php');
 
-$container = new Container();
-$container->singleton(\DDT\CLI::class, function() use ($argv){
-	$cli = new \DDT\CLI($argv);
-	$cli->enableErrors($cli->hasArg('--debug'));
-
-	return $cli;
-});
-$container->singleton(\DDT\Config\SystemConfig::class, function(){
-	return new \DDT\Config\SystemConfig($_SERVER['HOME']);
-});
-$container->singleton('tool-list', function(){
-	return array_map(function($t){ 
-		return ['name' => str_replace(['tool', '.php'], '', strtolower(basename($t))), 'path' => $t];
-	}, glob(__DIR__ . "/Tool/?*Tool.php"));
-});
-
-$cli = container(\DDT\CLI::class);
-
-\Shell::setDebug($cli->hasArg('debug'));
-\Text::setQuiet($cli->hasArg('quiet'));
-
-return $cli;
+return container(\DDT\CLI::class);
