fbf74956e30c08b38a4d1da900fd12657f9d5824
Chris Thomas <chris.thomas@antimatter-studios.com>
2021-08-28 20:46:00 +0200

    update to support running containers if the container by name wasn't already found

--- src/Docker/DockerContainer.php
+++ src/Docker/DockerContainer.php
@@ -2,17 +2,53 @@
 
 namespace DDT\Docker;
 
+use DDT\Exceptions\Docker\DockerContainerNotFoundException;
+
 class DockerContainer
 {
     private $docker;
 
-    public function __construct(Docker $docker)
+    private $name;
+
+    private $id;
+
+    public function __construct(Docker $docker, string $name, ?string $image = null, ?array $ports = [], ?array $volumes = [], ?array $options = [])
     {
         $this->docker = $docker;
+
+        $this->name = $name;
+
+        try{
+            $this->id = $this->getId();
+        }catch(DockerContainerNotFoundException $e){
+            if(empty($image)){
+                throw $e;
+            }
+
+            $this->docker->run($image, $name, $ports, $volumes, $options);
+
+            $this->id = $this->getId();
+        }
     }
 
     public function stop(): bool
     {
         return false;
+    }
+
+    public function getId(): string
+    {
+        try{
+            $id = $this->docker->inspect('container', $this->name, '-f \'{{ .Id }}\'');
+
+            return $id[0];
+        }catch(\Exception $e){
+            throw new DockerContainerNotFoundException($this->name);
+        }
+    }
+
+    public function exec(string $command)
+    {
+        return $this->docker->exec($this->id, $command, true);
     }
 }
