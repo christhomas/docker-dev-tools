3d501b7e2b1b4e1937cbf6d861c905cc1b429fae
Chris Thomas <chris.thomas@antimatter-studios.com>
2022-02-10 15:38:30 +0100

    rewrite the implementation to be much simpler

--- src/Text/Table.php
+++ src/Text/Table.php
@@ -7,6 +7,7 @@ class Table
     private $text;
 	private $data;
 	private $tabWidth = 2;
+	private $numColumns = 0;
 	private $space = " ";
 	private $rightPadding = 0;
 	private $debug = false;
@@ -16,13 +17,13 @@ class Table
         $this->text = $text;
 		$this->data = $data;
 		$this->tabWidth = $tabWidth;
+		$this->numColumns = 0;
 	}
 
 	public function setDebug(bool $state)
 	{
 		$this->debug = $state;
-
-		if($state) $this->space = "_";
+		$this->space = $state ? '_' : ' ';
 	}
 
 	public function debug($data)
@@ -40,32 +41,11 @@ class Table
     }
 
 	public function addRow($data) {
-		foreach($data as $c => $column){
-			$replace = str_pad("", $this->tabWidth, $this->space);
-			$data[$c] = str_replace("\t", $replace, $column);
+		if(count($data) > $this->numColumns){
+			$this->numColumns = count($data);
 		}
+
 		$this->data[] = $data;
-	}
-
-	private function adjustColumnWidthByNonPrintingChars($width, $text)
-	{
-		// Find all non text colour and non printing character codes
-		$codes = $this->text->findCodes($text);
-
-		$this->debug("Found ".count($codes)." codes");
-
-		$width = array_reduce($codes, function($c, $i){
-			// calculate the amount of characters we need to subtract
-			$l = strlen($i['value']);
-			// if character is printing, add back a number of characters (normally icons, are 2 characters in width)
-			$p = $i['printing'];
-			$subtract = $l - $p;
-			$new_c = $c - $subtract;
-			$this->debug("subtracting($subtract): '$c' -> '$new_c' hex(".bin2hex($i['value']).") or print({$i['value']}) ($l, $p)");
-			return $new_c;
-		}, $width);
-
-		return $width > 0 ? $width : 0;
 	}
 
 	public function setRightPadding(int $padding = 0)
@@ -73,80 +53,79 @@ class Table
 		$this->rightPadding = $padding;
 	}
 
+	private function fixColumnWidths($columns): array
+	{
+		// Replace tabs with spaces
+		$columns = array_map(function($text) {
+			$replace = str_pad("", $this->tabWidth, $this->space);
+			return str_replace("\t", $replace, $text);
+		}, $columns);
+
+		// Replace all special codes with shell script codes
+		$columns = array_map(function($text) {
+			return $this->text->write($text);
+		}, $columns);
+
+		// Find the printing character widths for every column
+		$pw = array_reduce($columns, function($accum, $text) {
+			$tw = strlen($text);
+			$np = 0;
+
+			$codes = $this->text->findCodes($text);
+			foreach($codes as $c){
+				$tw = $tw + $c['printing'];
+				$np = $np + $c['length'];
+			}
+
+			$accum[] = $tw - $np;
+			return $accum;
+		}, []);
+
+		// What is the minimum column width?
+		$min = max($pw);
+
+		// Determine the padding PER COLUMN, because each column contains a unique number of non-printing characters
+		$padding = array_reduce($columns, function($accum, $text) use ($pw, $min) {
+			$ci = count($accum);
+			$tw = strlen($text);
+			$np = $tw - $pw[$ci];
+		
+			$accum[] = $min + $np;
+			return $accum;
+		}, []);
+
+		// Pad each column according to it's own requirements
+		$columns = array_map(function($ci, $ct) use ($padding) {
+			return str_pad($ct, $padding[$ci] + $this->rightPadding, $this->space);
+		}, array_keys($columns), $columns);
+
+		return $columns;
+	}
+
 	public function render($buffer=false): ?string
 	{
 		if(empty($this->data)) return null;
 
-		if(count($this->data) > 1){
-			// This function rotates the array so that rows become columns and columns become rows
-			$transpose = function($array) {
-				array_unshift($array, null);
-				return call_user_func_array('array_map', $array);
-			};
+		$transpose = function($array) {
+			array_unshift($array, null);
+			return call_user_func_array('array_map', $array);
+		};
 
-			$flipped = $transpose($this->data);
-
-			// start with zeroed column widths
-			$colWidths = array_fill_keys(array_keys($flipped), 0);
-
-			foreach($flipped as $c => $column) {
-				// Every column starts with zero width
-				$max = $colWidths[$c];
-
-				foreach($column as $r => $string){
-					// Only process rows that have more than one column
-					// This ignores full width rows that span multiple columns
-					if(count($this->data[$r]) > 1){
-						// Get string length, adjust for hidden non printing characters
-						$cw = strlen($string);
-						$this->debug("col[$c] width before = $cw");
-						$cw = $this->adjustColumnWidthByNonPrintingChars($cw, $string);
-						$this->debug("col[$c] width after = $cw");
-						// If length is greater than the max for this column, set this as the max
-						if($cw > $max) $max = $cw;
-					}
-				}
-				$colWidths[$c] = $max;
-			}
-		}else{
-			foreach(current($this->data) as $c => $column){
-				$colWidths[$c] = strlen($column);
-			}
+		// Flip the array to process columns easily
+		$data = $transpose($this->data);
+		foreach($data as $index => $columns){
+			$data[$index] = $this->fixColumnWidths($columns);
 		}
 
-		$cellWidths = [];
-		foreach($this->data as $r => $row){
-			foreach($row as $c => $column){
-				// For every 'cell' calculate the amount of EXTRA characters we need to bring it to the required width
-				// We don't calculate the absolute width, that was not working, we calculate the EXTRA needed, it's more reliable
-				// it's hard to know how much to calculate when each cell can have different amounts of non-printing characters
-				$cw = strlen($column);
-				$cw = $this->adjustColumnWidthByNonPrintingChars($cw, $column);
-				$cellWidths[$r][$c] = $colWidths[$c] - $cw;
-			}
-		}
-		$this->var_dump(['widths' => $cellWidths]);
+		// Flip the array back to normal
+		$data = $transpose($data);
 
+		// Output the data as lines of strings
 		$output = "";
-		foreach($this->data as $r => $row){
-			$text = $row;
-
-			// If row has more than 1 column, we need to adjust the padding for each cell
-			if(count($row) > 1) {
-				$text = [];
-				foreach ($row as $c => $column) {
-					// pad the cell with the extra width we need
-					$width = strlen($column) + $cellWidths[$r][$c] + $this->rightPadding;
-					$column = str_pad($column, $width, $this->space);
-					// the column has the required EXTRA padding now
-					$text[] = $column;
-					$this->debug("row[$r] column[$c] width($width) length(".strlen($column).") = $column");
-				}
-			}
-
+		foreach($data as $row){
 			// we are done with this line now, finalise it's output somewheres
 			// implode, buffer, print
-			$row = implode(" ", $text) . "\n";
+			$row = implode(" ", $row) . "\n";
 
 			if($buffer){
 				$output .= $row;
