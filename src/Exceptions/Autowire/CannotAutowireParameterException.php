c15a959b16ce483647f55258be01da945bf57633
Chris Thomas <chris.thomas@antimatter-studios.com>
2022-02-18 13:04:09 +0100

    add functionality that will be useful for providing better feedback to the user

--- src/Exceptions/Autowire/CannotAutowireParameterException.php
+++ src/Exceptions/Autowire/CannotAutowireParameterException.php
@@ -3,7 +3,44 @@
 namespace DDT\Exceptions\Autowire;
 
 class CannotAutowireParameterException extends \Exception {
+    private $className;
+    private $methodName;
+    private $parameterName;
+    private $parameterType;
+
     public function __construct(string $name, string $type, int $code = 0, \Throwable $previous = null){
         parent::__construct("Could not autowire parameter '$name' because type '$type' was not autowirable or with a default value", $code, $previous);
+        $this->parameterName = $name;
+        $this->parameterType = $type;
+    }
+
+    public function setClassName(string $className): void
+    {
+        $this->className = $className;
+    }
+
+    public function getClassName(): string
+    {
+        return $this->className;
+    }
+
+    public function setMethodName(string $methodName): void
+    {
+        $this->methodName = $methodName;
+    }
+
+    public function getMethodName(): string
+    {
+        return $this->methodName;
+    }
+
+    public function getParameterName(): string
+    {
+        return $this->parameterName;
+    }
+
+    public function getParameterType(): string
+    {
+        return $this->parameterType;
     }
 }
