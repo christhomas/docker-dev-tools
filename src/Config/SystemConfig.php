794f33d4ef688c13b388b14507ce8a31420ef7fa
Chris Thomas <chris.thomas@antimatter-studios.com>
2022-02-19 07:56:02 +0100

    fix a problem writing the tools path, it should be double quotes

--- src/Config/SystemConfig.php
+++ src/Config/SystemConfig.php
@@ -34,7 +34,7 @@ class SystemConfig extends BaseConfig
 
 	public function setPath(string $name, string $path): void
 	{
-		$this->setKey('path.$name', $path);
+		$this->setKey("path.$name", $path);
 	}
 
 	public function getPath(string $name, ?string $subpath=''): string
@@ -46,49 +46,5 @@ class SystemConfig extends BaseConfig
 		}
 
 		return $path . $subpath;
-	}
-
-    public function listExtensions(): array
-	{
-		return $this->getKey("extensions");
-	}
-
-	public function addExtension(string $name, string $url, string $path): bool
-	{
-		$data = [
-			"url" => $url,
-			"path" => $path,
-		];
-
-		$this->setKey("extensions.$name", $data);
-
-		return count(array_diff($this->getKey("extensions.$name"), $data)) === 0;
-	}
-
-	public function removeExtension(string $name): bool
-	{
-		return $this->deleteKey("extensions.$name");
-    }
-
-    public function addProject(string $name, string $git, string $branch): bool
-	{
-        $this->setKey("projects.$name", [
-			"git" => $git,
-			"branch" => $branch
-		]);
-
-		return $this->hasProject($name);
-	}
-
-	public function removeProject(string $name): bool
-	{
-        $this->deleteKey("projects.$name");
-
-		return $this->hasProject($name) === false;
-	}
-
-	public function hasProject($name): bool
-	{
-        return $this->getKey("projects.$name") !== null;
 	}
 }
