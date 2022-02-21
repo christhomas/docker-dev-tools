73113bf8372dbc29e71e4b492e739843335ac625
Chris Thomas <chris.thomas@antimatter-studios.com>
2022-02-21 19:28:51 +0100

    add a method to obtain the container which was not found

--- src/Exceptions/Docker/DockerContainerNotFoundException.php
+++ src/Exceptions/Docker/DockerContainerNotFoundException.php
@@ -4,8 +4,16 @@ namespace DDT\Exceptions\Docker;
 
 class DockerContainerNotFoundException extends \Exception
 {
+    private $name;
+    
     public function __construct(string $name, int $code = 0, \Throwable $previous = null)
     {
         parent::__construct("Docker could not find the container '$name'", $code, $previous);
+        $this->name = $name;
+    }
+
+    public function getContainerName(): string
+    {
+        return $this->name;
     }
 }
