72f286c14d71ffa1c1217d6a3c60efe0baf5229d
Chris Thomas <chris.thomas@antimatter-studios.com>
2022-02-28 17:29:08 +0100

    this exception needs extra information to be useful

--- src/Exceptions/Project/ProjectScriptInvalidException.php
+++ src/Exceptions/Project/ProjectScriptInvalidException.php
@@ -3,8 +3,31 @@ namespace DDT\Exceptions\Project;
 
 class ProjectScriptInvalidException extends \Exception
 {
+    private $group;
+    private $project;
+    private $script;
+
     public function __construct(string $group, string $project, string $script, $code = 0, \Throwable $previous = null)
     {
         parent::__construct("The group '$group' and project '$project' does not have a valid commandline for script '$script'", $code, $previous);
+
+        $this->group = $group;
+        $this->project = $project;
+        $this->script = $script;
+    }
+
+    public function getGroup(): string
+    {
+        return $this->group;
+    }
+
+    public function getProject(): string
+    {
+        return $this->project;
+    }
+
+    public function getScript(): string
+    {
+        return $this->script;
     }
 };
